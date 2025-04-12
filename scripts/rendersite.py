ed650422b4571691883921df1ce1ee7721f49532
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-04-12 16:33:23 +0200

    Update script

--- scripts/rendersite.py
+++ scripts/rendersite.py
@@ -1,24 +1,45 @@
+from __future__ import annotations
+
 import sys
+import tomllib
+
+import typer
 from jinja2 import Template
 from pydantic import BaseModel
 
-class Link(BaseModel):
-    text: str
-    href: str
+app = typer.Typer()
 
-class TemplateVariables(BaseModel):
+
+class App(BaseModel):
+    title: str
+    dir: str
+
+
+class Site(BaseModel):
     title: str = "MapLibre for Python"
-    links: list[Link] = [
-        Link(text="Getting started", href="getting-started")
-    ]
+    apps: list[App] = [App(title="Getting started", dir="getting-started")]
 
-def render(filename: str) -> str:
-    with open(filename) as f:
-        template = Template(f.read())
+    @classmethod
+    def read_config(cls, filename: str) -> Site:
+        with open(filename, "rb") as f:
+            cfg = tomllib.load(f)
 
-    kwargs = TemplateVariables().model_dump()
-    return template.render(**kwargs)
+        return cls(**cfg["site"])
+
+    def render(self, filename: str) -> str:
+        with open(filename) as f:
+            template = Template(f.read())
+
+        return template.render(**self.model_dump())
+
+
+@app.command(help="Render site")
+def render(
+    input: str = "templates/site/index.html", config: str = "config/site.toml"
+) -> None:
+    site = Site.read_config(config)
+    print(site.render(input))
+
 
 if __name__ == "__main__":
-    filename = sys.argv[1] if len(sys.argv) > 1 else "templates/site/index.html"
-    print(render(filename))
+    app()
